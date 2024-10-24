e66f809e652ef2581b56f6cc04bf54a6b700bd13
rantbc <167193335+rantbc@users.noreply.github.com>
2024-10-24 19:35:07 +0300

    Вносим првки

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,15 @@
-function stringValidate (string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const stringValidate = (string, maxLength) => string.length <= maxLength ? true : false
 stringValidate('mynewstring', 20);
 // линтер ругается на консоль, проверял так: console.log(stringValidate('mynewstring', 20));
 
-function valindromCheck (string) {
-  let normalizeString = String(string);
-  normalizeString = normalizeString.replaceAll();
-  normalizeString = normalizeString.toLowerCase();
+function palindromCheck (string) {
+  const normalizeString = String(string).replaceAll(' ', '').toLowerCase();
   let emptyString = '';
   const stringLastSym = normalizeString.length - 1;
   for (let i = stringLastSym; i >= 0; i--) {
     emptyString += normalizeString[i];
   }
-  if (normalizeString === emptyString) {
-    return true;
-  } else {
-    return false;
-  }
+  return normalizeString === emptyString;
 }
-valindromCheck('топот');
+palindromCheck('топот');
 // линтер ругается на консоль, проверял так: console.log(valindromCheck('топот'));
